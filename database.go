a15fe7f34d1ec69908722eee608a099be4e4dd93
Vano2903 <davidevanoncini2003@gmail.com>
2022-03-05 00:05:12 +0100

    documented the code, still under development

--- database.go
+++ database.go
@@ -25,16 +25,18 @@ var (
 )
 
 type Controller struct {
-	ctx context.Context
-	cli *client.Client
+	ctx context.Context //context for the docker client
+	cli *client.Client  //docker client
 }
 
+//create a new controller
 func NewController() (*Controller, error) {
 	var err error
 
 	c := new(Controller)
 	c.ctx = context.Background()
 
+	//creating docker client from env
 	c.cli, err = client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		return nil, err
@@ -43,6 +45,8 @@ func NewController() (*Controller, error) {
 	return c, nil
 }
 
+//create a new database container given the db type, image, port, enviroment variables and volume
+//it returns the container id and an error
 func (c Controller) CreateNewDB(DbType, image, port string, env []string, volumePath string) (string, error) {
 	//pull the image, it wont pull it if it is already there
 	//it will update itself since if there is no tag by default it means latest
@@ -54,9 +58,7 @@ func (c Controller) CreateNewDB(DbType, image, port string, env []string, volume
 	//!could be used to sent to the client the output of the pull
 	// io.Copy(os.Stdout, out)
 
-	//!container configurations
-
-	//container config
+	//container config (image and environment variables)
 	config := &container.Config{
 		Image: image,
 		Env:   env,
@@ -83,6 +85,7 @@ func (c Controller) CreateNewDB(DbType, image, port string, env []string, volume
 	//set the configuration of the host
 	//set the port bindings and the restart policy
 	//!choose a restart policy
+	//!define the volume
 	hostConfig := &container.HostConfig{
 		PortBindings: portBinding,
 		RestartPolicy: container.RestartPolicy{
@@ -96,6 +99,7 @@ func (c Controller) CreateNewDB(DbType, image, port string, env []string, volume
 		// 		Target: "/var/lib/mysql",
 		// 	},
 		// },
+		//!should change, this is only for mysql
 		Binds: []string{
 			fmt.Sprintf("%s:/var/lib/mysql", volumePath),
 		},
@@ -116,20 +120,28 @@ func (c Controller) CreateNewDB(DbType, image, port string, env []string, volume
 	return resp.ID, nil
 }
 
+//get the first port opened by the container on the host machine
 func (c Controller) GetContainerExternalPort(id, containerPort string) (string, error) {
+	//same as docker inspect <id>
 	container, err := c.cli.ContainerInspect(c.ctx, id)
 	if err != nil {
 		return "", err
 	}
+	//from the network settings we get the port that the container is
+	//listening to internally and from there we get the host one
 	return container.NetworkSettings.Ports[nat.Port(fmt.Sprintf("%s/tcp", containerPort))][0].HostPort, nil
 }
 
+//search a volume by name and returns a pointer to the volume (type volumeType.Volume) and an error
+//if the volume doesn't exist the volume pointer will be nil
 func (c *Controller) FindVolume(name string) (volume *types.Volume, err error) {
+	//get all the volumes
 	volumes, err := c.cli.VolumeList(context.Background(), filters.NewArgs())
 	if err != nil {
 		return nil, err
 	}
 
+	//search the volume with the same name
 	for _, v := range volumes.Volumes {
 		if v.Name == name {
 			return v, nil
@@ -138,7 +150,10 @@ func (c *Controller) FindVolume(name string) (volume *types.Volume, err error) {
 	return nil, nil
 }
 
+//check if a volume exists, if so returns false, the volume and an error
+//if it doesn't exists it will be created and the output will be true, the volume and an error
 func (c *Controller) EnsureVolume(name string) (created bool, volume *types.Volume, err error) {
+	//check if the volume exists (if it doesn't volume will be nil)
 	volume, err = c.FindVolume(name)
 	if err != nil {
 		return false, nil, err
@@ -148,6 +163,7 @@ func (c *Controller) EnsureVolume(name string) (created bool, volume *types.Volu
 		return false, volume, nil
 	}
 
+	//create the volume given the context and the volume create body struct
 	vol, err := c.cli.VolumeCreate(c.ctx, volumeType.VolumeCreateBody{
 		Driver: "local",
 		Labels: map[string]string{"matricola": "18008", "type": "db", "dbType": "mysql"},
@@ -156,7 +172,9 @@ func (c *Controller) EnsureVolume(name string) (created bool, volume *types.Volu
 	return true, &vol, err
 }
 
+//delete a volume (only if the volume exists, if it doesnt the function will return false)
 func (c *Controller) RemoveVolume(name string) (removed bool, err error) {
+	//search the volume
 	vol, err := c.FindVolume(name)
 	if err != nil {
 		return false, err
@@ -166,6 +184,7 @@ func (c *Controller) RemoveVolume(name string) (removed bool, err error) {
 		return false, nil
 	}
 
+	//remove the volume
 	err = c.cli.VolumeRemove(context.Background(), name, true)
 	if err != nil {
 		return false, err
