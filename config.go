e79ee39d284f680abd7d2eeb504ffa212ef3722d
Vano2903 <davidevanoncini2003@gmail.com>
2022-03-05 00:04:46 +0100

    added documentation of the code

--- config.go
+++ config.go
@@ -16,16 +16,19 @@ var (
 )
 
 func init() {
+	//load the enviroment variables
 	err := godotenv.Load(".env")
 	if err != nil {
 		panic("Error loading .env file")
 	}
 
+	//generate private and public keys
 	privateKey, err = rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		panic(err)
 	}
 	publicKey = &privateKey.PublicKey
 
+	//generate the session storage
 	store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
 }
