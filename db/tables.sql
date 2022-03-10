12d31471dcd6b8f995fb2ed8e3dda22a1d818822
Vano2903 <davidevanoncini2003@gmail.com>
2022-03-10 12:52:09 +0100

    added user and tokens tables

--- db/tables.sql
+++ db/tables.sql
@@ -8,3 +8,21 @@ CREATE TABLE IF NOT EXISTS `states` (
     `issDate` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `tokens` (
+    `userID` int not null,
+    `accToken` char(64) NOT NULL,
+    `accExp` DATETIME NOT NULL,
+    `refreshToken` char(64) NOT NULL,
+    `refreshExp` DATETIME NOT NULL,
+    PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `users`(
+    `userID` int not null,
+    `name` varchar(30) NOT NULL,
+    `lastName` varchar(30) NOT NULL,
+    `email` varchar(75) NOT NULL,
+    `pfp` varchar(55) NOT NULL,
+    PRIMARY KEY(`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
