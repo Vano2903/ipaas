5f53d5c949dad128b0a88aa82506e94c7d088f6a
vano <davidevanoncini2003@gmail.com>
2022-04-10 01:34:01 +0200

    added endpoints and allowed methods

--- main.go
+++ main.go
@@ -19,17 +19,32 @@ type Payload struct {
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/oauth", handler.OauthHandler)
+	r.HandleFunc("/oauth", handler.OauthHandler).Methods("GET")
+	r.HandleFunc("/tokens/new", handler.NewTokenPairFromRefreshTokenHandler).Methods("GET")
+	r.HandleFunc("/{studentID}/all", handler.GetAllApplicationsOfStudentPublic).Methods("GET")
 
 	//user's router with access token middleware
 	userAreaRouter := r.PathPrefix("/user").Subrouter()
 	//set middleware on user area router
 	userAreaRouter.Use(handler.TokensMiddleware)
-	userAreaRouter.HandleFunc("/login", handler.LoginHandler)
+	//get the user data
+	//still kinda don't know what to do with this one, will probably return the homepage
+	userAreaRouter.HandleFunc("/", handler.LoginHandler).Methods("GET")
+	//get all the applications (even the private one) must define the type (database, web, all)
+	userAreaRouter.HandleFunc("/getApps/{type}", handler.GetAllApplicationsOfStudentPrivate).Methods("GET")
 
-	//dbaas router (subrouter of user area router so it has access token middleware)
+	//DBaaS router (subrouter of user area router so it has access token middleware)
 	dbRouter := userAreaRouter.PathPrefix("/db").Subrouter()
-	dbRouter.HandleFunc("/new", handler.NewDBHandler)
+	//let the user create a new database
+	dbRouter.HandleFunc("/new", handler.NewDBHandler).Methods("POST")
+	//delete a database
+	dbRouter.HandleFunc("/delete/{containerID}", handler.DeleteApplicationHandler).Methods("DELETE")
+	// dbRouter.HandleFunc("/export/{containerID}/{dbName}")
+
+	//application router, it's the main part of the application
+	appRouter := userAreaRouter.PathPrefix("/app").Subrouter()
+	// appRouter.HandleFunc("/new", handler.NewAppHandler)
+	appRouter.HandleFunc("/delete/{containerID}", handler.DeleteApplicationHandler).Methods("DELETE")
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
