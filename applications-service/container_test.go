2a1d64cbc3cc20b4a51bf08e72d0c7f6f0dff478
Vano2903 <davidevanoncini2003@gmail.com>
2022-08-13 15:45:07 +0200

    testing file

--- applications-service/container_test.go
+++ applications-service/container_test.go
@@ -1,13 +1,48 @@
 package main
 
-// func TestCreateImage(t *testing.T) {
-// 	controller, err := NewContainerController(context.Background())
-// 	if err != nil {
-// 		t.Fatalf("error creating the new controller: %v", err)
-// 	}
+import (
+	"context"
+	"testing"
 
-// 	creatorID := "18008" //valid creator id
-// 	port := 8080         //webserver port
-// 	name := "test"       //app name
-// 	path := "test"       //path to the tmp file
-// }
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateImage(t *testing.T) {
+	assert := assert.New(t)
+
+	controller, err := NewContainerController(context.Background())
+	if err != nil {
+		t.Fatalf("error creating the new controller: %v", err)
+	}
+
+	urls := []struct {
+		Url         string
+		Branch      string
+		Envs        []Env
+		ShouldError bool
+	}{
+		{"https://github.com/vano2903/testing", "master", []Env{}, false},
+		// {"github.com/vano2903/testing", "master", []Env{{Key: "TestKey", Value: "testValue"}}, false},
+		{"github.com/vano2903/testing", "non-working-version", []Env{}, true},
+		// {"github.com/vano2903/testing", "runtime-error", []Env{}, false},
+		// {"github.com/vano2903/testing", "inexisting-branch", []Env{}, true},
+	}
+	userID := 10000
+	port := 8080 //webserver port
+
+	for _, url := range urls {
+		userID++
+		path, name, _, _ := DownloadGithubRepo(userID, url.Branch, url.Url)
+		_, _, err = controller.CreateImage(userID, port, name, path, "go", url.Envs)
+		if url.ShouldError {
+			assert.Error(err)
+		} else {
+			assert.NoError(err)
+		}
+	}
+
+	t.Cleanup(func() {
+		//delete created images
+		//delete downloaded folders
+	})
+}
