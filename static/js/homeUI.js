8f8d375bfcbe713e7272331b7626b7d6f2a27125
vano <davidevanoncini2003@gmail.com>
2022-10-02 21:18:38 +0200

    changed cookies name

--- static/js/homeUI.js
+++ static/js/homeUI.js
@@ -19,10 +19,10 @@ function transition(from, to) {
 
 function logout() {
     //remove accessToken and refreshToken from cookie
-    document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "ipaas-access-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "ipaas-refresh-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.href = "/login";
-};
+}
 
 function logoutconfirm(x) {
     if (x) {
@@ -32,12 +32,17 @@ function logoutconfirm(x) {
         document.querySelector("#logoutconfirm").style.display = "none";
         console.log("nasc");
     }
-};
+}
 
 /*######## add document #########*/
 let lastPressed;
 
 $("#inputFile").css('display', 'none');
+
+function adddoc(type) {
+    lastPressed = type;
+    $("#inputFile").trigger('click');
+}
 
 function adddoctoggle() {
     document.querySelector("#adddoc").classList.toggle("open");
@@ -51,7 +56,32 @@ function adddoctoggle() {
     }
 }
 
-function adddoc(type) {
-    lastPressed = type;
-    $("#inputFile").trigger('click');
+function addEnv() {
+    const env = document.createElement("div");
+    env.className = "env";
+    env.id = "env" + document.querySelectorAll(".env").length;
+    //put the two input inline
+    env.innerHTML = `
+        <div class="row">
+            <div class="col-5">
+                <input type="text" class="form-control env-key" placeholder="Chiave" required>
+            </div>
+            <div class="col-5">
+                <input type="text" class="form-control env-value" placeholder="Valore" required>
+            </div>
+            <div class="col-1">
+                <button class="btn btn-danger" onclick="removeEnv('${env.id}')">X</button>
+            </div>
+        </div>
+        <br>
+
+<!--        // <input type="text" placeholder="Nome variabile" class="form-control envName">-->
+<!--        // <input type="text" placeholder="Valore variabile" class="form-control envValue">-->
+<!--        // <button onclick="removeEnv('${env.id}')">X</button>-->
+    `;
+    document.querySelector("#env-container").appendChild(env);
+}
+
+function removeEnv(id) {
+    document.querySelector("#" + id).remove();
 }
